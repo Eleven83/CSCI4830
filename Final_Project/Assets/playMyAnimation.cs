2d07c15542aa17ac45a29a89946b967e1045e8db
Ryan Hoffman <ryho8890@colorado.edu>
2019-12-07 18:14:29 -0700

    Finished FPS for game view camera movement.
    As the summary says.

---
+++ Final_Project/Assets/playMyAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playMyAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator myAnimationController;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            myAnimationController.SetBool("playRock", true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            myAnimationController.SetBool("playRock", false);
+        }
+    }
+}
